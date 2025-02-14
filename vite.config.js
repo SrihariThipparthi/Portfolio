0d14a0987af792a42991d96c3f62ade60be877e6
SrihariThipparthi <thipparthisrihari2@gmail.com>
2025-02-14 14:03:36 +0530

    updated configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // Add this line - should match your repository name
   root: './',  // Points to the root directory where index.html is located
   build: {
     outDir: 'dist'
